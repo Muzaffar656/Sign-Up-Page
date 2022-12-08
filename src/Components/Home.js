2c4b8e5671fe80fa6ac31c3fa9fe9f74ebdeb7bb
Muzaffar656 <106021084+Muzaffar656@users.noreply.github.com>
2022-12-08 02:49:06 -0800

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css"
+
+const Home = ()=>{
+    return (
+        <>
+        <div className="login-bar">
+            <h1>Login</h1>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Passwored" />
+            <div className="login-btn">Login</div> 
+            <div className="alt-login">
+                <div className="google"></div>
+                <div className="facebook"></div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Home;
